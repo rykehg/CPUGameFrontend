370f3b6385950095c0a6c22f5ff331ba5743010d
Henrique Gaeta <henriquehg@gmail.com>
2020-06-19 21:04:42 -0300

    adicionando

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -6,8 +6,6 @@ import api from '../../services/api';
 
 import './styles.css'
 
-import logoImg from '../../assets/logo.svg';
-
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
 
@@ -49,7 +47,6 @@ export default function Profile() {
   return (
     <div className="profile-container">
       <header>
-        <img src={logoImg} alt="Be The Hero" />
         <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
